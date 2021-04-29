7aa2082d601fa7281d96bad4f7992deb19172f5f
Luis Alexandre Rodrigues <luis.rodrigues@aarin.com.br>
2021-04-29 16:34:48 -0300

    Initial Setup

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace FeatureFlagSampleAPI
 {
@@ -19,8 +15,15 @@ namespace FeatureFlagSampleAPI
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
+                webBuilder.ConfigureAppConfiguration(config =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var settings = config.Build();
+
+                config.AddAzureAppConfiguration(options =>
+                    options.Connect(settings["AppConfig"]).UseFeatureFlags(featureFlagOptions => {
+                        featureFlagOptions.CacheExpirationInterval = TimeSpan.FromMinutes(5);
+                    }));
+
+                }).UseStartup<Startup>());
     }
 }
